72abce9fef7033ab5d3574472ce74d57c2c96252
Sicheng Chen <sicheng@win2.cis.ksu.edu>
2023-03-21 19:08:21 -0500

    Added data mapping to fe_Projects

--- components/Fe_Projects.tsx
+++ components/Fe_Projects.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { createStyles, Center, Container, Grid, Paper, Group, Badge, Button, Card, Image, Text } from '@mantine/core';
+import { createStyles, Center, Container, Grid, Paper, Group, Badge, Button, Card, Image, Text, CardSection } from '@mantine/core';
 import { Carousel } from '@mantine/carousel';
+import Link from 'next/link';
 
 const useStyles = createStyles(() => ({
 
@@ -19,9 +20,66 @@ const useStyles = createStyles(() => ({
 
 }));
 
+const elements = [
+  { 
+    image: 'res/projects/MapViewer.png',
+    name: 'Map Viewer',
+    status: 'Complete',
+    link: 'https://github.com/StrykerPX/Map-Viewer'
+  },
+
+  { 
+    image: 'res/projects/chroot.png',
+    name: 'Chroot',
+    language: 'Javascript',
+    type: 'Bot',
+    status: 'In progress',
+    link: 'https://github.com/StrykerPX/Chroot'
+  },
+
+  { 
+    image: 'res/projects/acm_website.png',
+    name: 'ACM Website',
+    status: 'Complete',
+    link: 'https://acm.cs.ksu.edu/'
+  },
+];
+
 export function Fe_Prjects() {
 
   const { classes } = useStyles();
+
+  const projects = elements.map((element) => (
+    <Carousel.Slide>
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <CardSection>
+        <Image
+          src={element.image}
+          height={160}
+          alt="Norway"
+        />
+        </CardSection>
+        <Group position="apart" mt="md" mb="xs">
+          <Text weight={500}>{element.name}</Text>
+          <Badge  color={(element.status === 'Complete') ? 'teal' : 'blue'}>
+            {element.status}
+          </Badge>
+        </Group>
+
+        <Text size="sm" color="dimmed">
+          With Fjord Tours you can explore more of the magical fjord landscapes with tours and
+          activities on and around the fjords of Norway
+        </Text>
+          <Link
+            href={element.link}
+          >
+            <Button variant="light" color="blue" fullWidth mt="md" radius="md">
+              View project
+            </Button>
+          </Link>
+      </Card>
+    </Carousel.Slide>
+  ));
 
   return (
     <>
@@ -32,83 +90,7 @@ export function Fe_Prjects() {
             height={350}
             slideGap="xl" loop
           >
-            <Carousel.Slide>
-              <Card shadow="sm" padding="lg" radius="md" withBorder>
-                <Card.Section>
-                  <Image
-                    src="MapViewer.png"
-                    height={160}
-                  alt="Norway"
-                  />
-                </Card.Section>
-                <Group position="apart" mt="md" mb="xs">
-                  <Text weight={500}>Map Viewer</Text>
-                  <Badge color="teal" variant="light">
-                    Complete
-                  </Badge>
-                </Group>
-
-                <Text size="sm" color="dimmed">
-                  MapViewer is a .NET Framework application that displays map of a particular location using QuadTree data structure by taking CSV file as an input.
-                </Text>
-
-                <Button variant="light" color="blue" fullWidth mt="md" radius="md">
-                  View project
-                </Button>
-              </Card>
-            </Carousel.Slide>
-            <Carousel.Slide>
-              <Card shadow="sm" padding="lg" radius="md" withBorder>
-                <Card.Section>
-                  <Image
-                    src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
-                    height={160}
-                  alt="Norway"
-                  />
-                </Card.Section>
-                <Group position="apart" mt="md" mb="xs">
-                  <Text weight={500}>Project Name</Text>
-                  <Badge color="teal" variant="light">
-                    Complete
-                  </Badge>
-                </Group>
-
-                <Text size="sm" color="dimmed">
-                  With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-                  activities on and around the fjords of Norway
-                </Text>
-
-                <Button variant="light" color="blue" fullWidth mt="md" radius="md">
-                View project
-                </Button>
-              </Card>
-            </Carousel.Slide>
-            <Carousel.Slide>
-              <Card shadow="sm" padding="lg" radius="md" withBorder>
-                <Card.Section>
-                  <Image
-                    src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
-                    height={160}
-                  alt="Norway"
-                  />
-                </Card.Section>
-                <Group position="apart" mt="md" mb="xs">
-                  <Text weight={500}>Project Name</Text>
-                  <Badge color="teal" variant="light">
-                  Complete
-                  </Badge>
-                </Group>
-
-                <Text size="sm" color="dimmed">
-                  With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-                  activities on and around the fjords of Norway
-                </Text>
-
-                <Button variant="light" color="blue" fullWidth mt="md" radius="md">
-                View project
-                </Button>
-              </Card>
-            </Carousel.Slide>
+            {projects}
           </Carousel>
         </Container>
       </div>  
